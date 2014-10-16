5236f4eec86e6dd46bbdba5cdb8436d1c3a0b42b
Robin Gowin <landr100@gmail.com>
2014-10-16 10:51:31 -0400

    removed obsolete code and comments

--- python/utils.py
+++ python/utils.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 
 # utils.py
-# similar in purpose to utilities.rb; general purpose utilities
+# general purpose utilities
 
 import glob # glob.glob()
 import os, sys # os.stat(), sys.argv[], sys.stderr.write(), os.system()
-# from stat import * # stat.ST_SIZE
 
 class Name1(object):
 	def __init__(self):
@@ -19,7 +18,6 @@ class Name1(object):
 		self.Method1(to_print)
 
 def print_files(files="*.py"):
-	# interesting iterators
 	filelist = glob.glob(files)
 	numfiles = len(filelist)
 	if numfiles > 0:
@@ -31,36 +29,9 @@ def print_files(files="*.py"):
 
 		print file_stats
 
-## append to a list
-#filelist.append('dummy')
-
-#print filelist
-
-## for loop
-#for file in filelist:
-	#print file
-
-## range builtins
-#for i in xrange(10):
-	#print i
-
-#print range(11)
-
-#import time
-#print "sleeping"
-#time.sleep(3)
-#print "awake"
-
-#print time.time() # => 1378323705.22
-import time
+import time # print time.time() # => 1378323705.22
 My_now = time.strftime('%F %T') # 2014-09-14 16:16:16
 My_today = time.strftime('%F') # 2014-09-14
-
-#os.mkdir('./tmp')
-#os.chdir('./tmp')
-#print os.getcwd()
-#os.chdir('..')
-#print os.getcwd()
 
 # mimic the ruby notion of global variables in python
 One_minute = 60
@@ -68,20 +39,20 @@ One_hour = 60 * One_minute
 One_day = One_hour * 24
 
 import socket
-My_host = socket.gethostname() # => 'Robins-MacBook-Pro.local' # ref: http://stackoverflow.com/questions/4271740/how-can-i-use-python-to-get-the-system-hostname
+My_host = socket.gethostname()
 
 # My_script should be the name of the script that imports this one
 My_script = os.path.basename(sys.argv[0]) # ref: http://stackoverflow.com/questions/4152963/get-the-name-of-current-script-with-python
 
 # example of using string match with regex
-# if re.search('cdb|ampapp|ampdb', My_host) is not None:
 import re
-# this is to remember more easily
+# simple method to search for a regex in a string
 def find(regex, string):
 	return re.search(regex, string) is not None
 
-Is_home_mac = (True if find('iMac|Macintosh', My_host) else False)
-Is_mac = (True if 'Mac' in My_host else False)
+# customize if running on a mac
+Is_home_mac = (True if find('iMac|[mM]acintosh|macintodd', My_host) else False)
+Is_mac = (True if find('[mM]ac', My_host) or Is_home_mac else False)
 
 import time # time.strftime()
 
@@ -94,10 +65,10 @@ def log(str):
 	print "%s : %s" % (time.strftime(Time_format), str)
 	sys.stdout.flush() # ref: http://stackoverflow.com/questions/230751/how-to-flush-output-of-python-print
 
-# alias echo log
+# echo is an alias for log
 echo = log
-# puts = print # fails
-# echo('test')
+
+# lower case versions of constants
 false = False
 true = True
 nil = None
@@ -121,10 +92,8 @@ def run_cmd(cmd, echo=False):
 	p = subprocess.Popen([cmd], shell=True, bufsize=3000006, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
 	(child_stdout, child_stdin) = (p.stdout, p.stdin)
 	# >>> print child_stdout.readlines()
-	# ['atest.py\n', 'atest.pyc\n', 'combine.csharp\n', 'combine.csharp.source\n', 'combine.java\n', 'combine.java.source\n', 'combine1.rb\n',
-	# 'combine2-test.rb\n', 'combine2.rb\n', 'combine3.rb\n', 'combine4.rb\n', 'combine_test.java\n', 'gen-tcs.rb\n', 'junk.notes\n', 'junk.notes.old\n',
-	# ..., 'x.rb\n']
-	result = ''.join([line for line in child_stdout]) # this one does chomp but probably we need the newlines
+	# ['atest.py\n', 'atest.pyc\n', ..., 'x.rb\n']
+	result = ''.join([line for line in child_stdout]) # this one does chomp but we need the newlines
 	child_stdout.close()
 	p.stdout.close()
 
@@ -133,63 +102,19 @@ def run_cmd(cmd, echo=False):
 
 	return result
 
-## borrowed from daily.rb which borrowed from lib/config.rb on dlp1 and modified
-#def parse_opts
-#	if @opts
-#		log "in parse_opts: @opts already defined; returning"
-#		return
-#	end
-#
-#	@opts = {}
-#	ARGV.delete_if do |x|
-#		rv = false # tells delete_if whether to delete this arg
-#		if x[/^-{1,2}([^\s=]+)=(.*)/] # match simple --name='some value' without the quotes
-#			@opts[$1] = $2
-#			rv = true
-#		elsif x[/^-{1,2}([^\s=]+)/]
-#			@opts[$1] = 1
-#			rv = true
-#		end
-#		rv
-#	end
-#end
-import optparse # optparse.OptionParser()
-def old_parse_opts():
-	return 0
-
-	# note: these are just examples; the approach in python is different from ruby; no global hash
-
-	# ref: http://stackoverflow.com/questions/20063/whats-the-best-way-to-grab-parse-command-line-arguments-passed-to-a-python-scri
-
-	parser = optparse.OptionParser()
-
-	parser.add_option('-q', '--query', action="store", dest="query", help="query string", default="spam")
-
-	options, args = parser.parse_args()
-
-	print 'Query string:', options.query
-
-	echo("to be continued...")
-
 import time
 # this is like an alias
 def sleep(delay):
 	time.sleep(delay)
 
 # ref: http://code.activestate.com/lists/python-list/259063/
+# implement a version of perl and ruby 'chomp'
 def chomp(s):
 	if s[-1:]=='\n': return s[:-1]
 	else: return s
 
-# ref: comment embedded in web site http://stackoverflow.com/questions/2554185/match-groups-in-python
-# "Caveat: Python re.match() specifically matches against the beginning of the target.
-# Thus re.match("I love (\w+)", "Oh! How I love thee") would NOT match.
-# You either want to use re.search() or explicitly prefix the regex with appropriate wildcard patterns for re.match(".* I love (\w+)", ...)"
-
 # simulate File.exists?()
 class File(str):
-	#def __init__(self, my_str):
-	#  self._str = my_str
 	def exists(self):
 		return os.path.exists(self._str)
 	def exists(self, file):
@@ -199,26 +124,12 @@ class File(str):
 	def exist(self, file):
 		return os.path.exists(file)
 
-if __name__ == "__main__":
-	# main(unused_arg):
-	myvar = Name1()
-	myvar.Method2()
-	myvar.Method2()
-
-	# try again
-	Name1().Method2(False)
-	Name1().Method2(False)
-
-	log("in utils.py; to test it, run test-utils.py")
-
 # return true or false if a command exists
 def is_cmd(str):
 	result = run_cmd("which %s 2>/dev/null | grep -c %s" % (str, str))
 	return int(chomp(result)) == 1
 
-# from interrogate import *
-# def interrogate(item):
-
+# hour minute second
 def hms(input_sec):
 	sec = float(input_sec)
 	d = int(sec / One_day);    sec = sec % One_day
@@ -233,16 +144,12 @@ def hms(input_sec):
 
 # append a line to a file
 def append_line(filename, line):
-	# File.open(filename, 'a') { |f| f.puts line }
 	with open(filename, "ab") as fp:
 		fp.write(line + '\n')
 
-# return a string with the current stack
+# return a string with the current run-time stack
 def print_stack():
 	import traceback
-	# traceback.print_stack()
-	# for line in traceback.format_stack():
-	# print line.strip()
 	my_str = ''.join(traceback.format_stack())
 	return re.sub('  ', ' ', re.sub('\n', ';', my_str))
 
@@ -257,6 +164,17 @@ def dlog(msg):
 # modified implementation of split which returns an empty list if the input is empty
 def mysplit(s, delim=None):
 	return [x for x in s.split(delim) if x]
+
+if __name__ == "__main__":
+	myvar = Name1()
+	myvar.Method2()
+	myvar.Method2()
+
+	# try again
+	Name1().Method2(False)
+	Name1().Method2(False)
+
+	log("in utils.py; to test it, run test-utils.py")
 
 # end of file
 
